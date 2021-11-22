a0cd033fc675858c677401b712e8ce0c4fbc67e6
Pascal Ellenberger <pascal.ellenberger@protonmail.com>
2021-11-22 13:02:28 +0100

    fixed linter errors

--- alarm.py
+++ alarm.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 
-from gpiozero import TonalBuzzer
-from gpiozero.tones import Tone
-from signal import pause
-from rpi_TM1638 import TMBoards
 import threading
 import time
+from gpiozero import TonalBuzzer
+from rpi_TM1638 import TMBoards
 from my_logger import get_logger
 
 speaker = 2 #gpio
@@ -19,7 +17,7 @@ def start_alarm():
     buzzer.play("C4")
     logger = get_logger("start_alarm")
     logger.info("started playing")
-        
+
 def stop_alarm():
     buzzer.stop()
     buzzer.close()
@@ -28,24 +26,24 @@ def stop_alarm():
 
 def handle_input():
     brightness_max = 7
-    tm = TMBoards(tm_diode, tm_clock, tm_strobe, brightness_max)
-    i = tm.getData(None)
+    tm_board = TMBoards(tm_diode, tm_clock, tm_strobe, brightness_max)
+    i = tm_board.getData(None)
     logger = get_logger("handle_input")
     while i == default_buttons:
-        i = tm.getData(None)
-        logger.info("read i: [" + " ".join(str(n) for n in i ) + "]")
+        i = tm_board.getData(None)
+        logger.info(f"read i: [{0}]".format(" ".join(str(n) for n in i)))
         if i != default_buttons:
             stop_alarm()
         time.sleep(0.1)
 
 if __name__ == "__main__":
-    logger = get_logger("main")
-    logger.info('started script')
+    main_logger = get_logger("main")
+    main_logger.info('started script')
     # todo: try out https://docs.python.org/3/library/asyncio.html
-    alarm_t =threading.Thread(target=start_alarm)
-    logger.info('instantiated thread')
+    alarm_t = threading.Thread(target=start_alarm)
+    main_logger.info('instantiated thread')
     alarm_t.start()
-    logger.info('started thread')
+    main_logger.info('started thread')
     handle_input()
     alarm_t.join()
-    logger.info("done")
+    main_logger.info("done")
--- my_logger.py
+++ my_logger.py
@@ -7,14 +7,14 @@ import sys
 FORMATTER = logging.Formatter("%(asctime)s — %(name)s — %(levelname)s — %(message)s")
 
 def get_console_handler():
-   console_handler = logging.StreamHandler(sys.stdout)
-   console_handler.setFormatter(FORMATTER)
-   return console_handler
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(FORMATTER)
+    return console_handler
 
 def get_logger(logger_name):
-   logger = logging.getLogger(logger_name)
-   logger.setLevel(logging.DEBUG) # better to have too much log than not enough
-   logger.addHandler(get_console_handler())
-   # with this pattern, it's rarely necessary to propagate the error up to parent
-   logger.propagate = False
-   return logger
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.DEBUG) # better to have too much log than not enough
+    logger.addHandler(get_console_handler())
+    # with this pattern, it's rarely necessary to propagate the error up to parent
+    logger.propagate = False
+    return logger
